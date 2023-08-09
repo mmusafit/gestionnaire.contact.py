a0c52fac301b53d33a6f732ca6a7fca39216d5e0
mmusafit <118737447+mmusafit@users.noreply.github.com>
2023-08-09 02:56:20 +0200

    Update gestionnaire.contact.py

--- gestionnaire.contact.py
+++ gestionnaire.contact.py
@@ -45,23 +45,48 @@ class Contact:
 
 class ListeContacts:
     def __init__(self):
-        self.contact = None
+        self.contact = None  # Initialisation de la liste vide
 
     def is_empty(self):
+        """
+        Vérifie si la liste de contacts est vide.
+
+        Returns:
+            bool: True si la liste est vide, False sinon.
+        """
         return self.contact is None
 
     def add(self, item):
+        """
+        Ajoute un nouveau contact à la liste.
+
+        Args:
+            item (dict): Dictionnaire contenant les informations du contact.
+
+        Returns:
+            None
+        """
         temp = Contact(item['nom'], item['prenom'], item['numero_telephone'], item['adresse_email'])
-        temp.setnext(self.contact)  # Utilisez "setnext" au lieu de "set_next"
+        temp.setnext(self.contact)  # Ajout du contact à la tête de la liste
         self.contact = temp
 
     def add_after(self, base, item):
+        """
+        Ajoute un contact après un contact de base dans la liste.
+
+        Args:
+            base (Contact): Contact après lequel ajouter le nouveau contact.
+            item (dict): Dictionnaire contenant les informations du nouveau contact.
+
+        Returns:
+            None
+        """
         temp = Contact(item['nom'], item['prenom'], item['numero_telephone'], item['adresse_email'])
-        temp.set_next(base.get_next())
+        temp.set_next(base.get_next())  # Réarrangement des pointeurs
         base.set_next(temp)
 
 
-# Création de quelques contacts
+# Création de quelques contacts au format de dictionnaire
 Contact1 = {"nom": "Doe", "prenom": "John", "numero_telephone": "123456789", "adresse_email": "john@example.com"}
 Contact2 = {"nom": "Smith", "prenom": "Jane", "numero_telephone": "987654321", "adresse_email": "jane@example.com"}
 
